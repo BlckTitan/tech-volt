a3cab41395c967d49130f80fdae7c6c5a07360c3
BlckTitan <dorshman406@gmail.com>
2025-03-26 15:53:57 +0100

    Feat(membershipComponent): added membership section with promotional cards and image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Navigation from '@/component/navigation/Navigation';
 import Carousel from '@/component/carousel/Carousel';
+import Membership from '@/component/membership/Membership';
+import ForYou from '@/component/forYou/ForYou';
 
 export default function Home() {
   return (
     <div className='w-full h-screen'>
       <Navigation/>
       <Carousel/>
+      <Membership/>
+      <ForYou/>
     </div>
   );
 }
---
+++ src/component/membership/Membership.jsx
@@ -0,0 +1,47 @@
+'use client'
+import React from 'react'
+import './membership.css'
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle, } from "@/components/ui/card"
+// image import
+import AC from '../../../public/img/image-AC.webp'
+import washingMachine from '../../../public/img/image-washing-machine.webp'
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+  
+const data =[
+    {id: 1, title: 'Fantastic deals await!', description:'Do not miss our promotions on home appliances, audio and video products', image: AC},
+    {id: 2, title: 'Be an TechVolt member', description:'Enjoy all the membership benefits, from special discounts to exclusive services and offers', image: washingMachine},
+    {id: 3, title: 'Gift registration', description:'Check out redemption procedures for promotional gifts', image: AC},
+    {id: 4, title: 'Be an TechVolt member', description:'Enjoy all the membership benefits, from special discounts to exclusive services and offers', image: washingMachine},
+]
+
+export default function MembershipComponent() {
+
+  return (
+    <section className='w-full h-fit !py-6 md:h-screen bg-cyan-100 flex flex-col justify-center items-center'>
+
+        <header className='w-9/12 h-24 flex justify-start items-start'>
+            <h1 className='component-title'>Membership</h1>
+        </header>
+
+        <div className='w-9/12 md:h-6/12 flex flex-col md:flex-row justify-between items-center'>
+
+            {data.map((item, index) => (
+                <Card key={index} className='w-80 h-96 !p-4 !mt-3 md:mt-0 bg-white border border-gray-50'>    
+                    <CardHeader className='w-full h-5/12'>
+                        <Image src={item.image} alt={item.title} className='w-full h-full object-cover object-center'/>
+                    </CardHeader>
+                    <CardContent className='w-full h-5/12 '>
+                        <CardTitle className='card-title'>{item.title}</CardTitle>
+                        <CardDescription className='card-description'>{item.description}</CardDescription>
+                    </CardContent>
+                    <CardFooter className='w-full h-2/12 flex items-center'>
+                        <Button variant="outline" className='!p-2 text-white bg-red-400 hover:bg-red-500'>Buy Now</Button>
+                    </CardFooter>
+                </Card>
+            ))}
+
+        </div>
+    </section>
+  )
+}
