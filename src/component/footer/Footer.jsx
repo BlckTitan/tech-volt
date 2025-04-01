b3786d92b9e6be539b1ef4d53ac02a959a86d445
BlckTitan <dorshman406@gmail.com>
2025-04-01 22:35:08 +0100

    Feat(FooterComponent): Updated footer layout for improved responsiveness; adjusted width classes for better display on larger screens

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -7,14 +7,14 @@ import './footer.css'
 export default function FooterComponent() {
 
   return (
-    <footer className='footer-component w-full h-fit md:h-[30vh] !py-6 bg-amber-100 border-t border-solid border-amber-200 flex flex-col justify-center items-center'>
+    <footer className='footer-component w-full h-fit xl:h-[30vh] !py-6 bg-amber-100 border-t border-solid border-amber-200 flex flex-col justify-center items-center'>
 
-      <section  className='w-full md:w-9/12'>
+      <section  className='w-full xl:w-9/12'>
         
-        <section className='w-full h-fit md:h-[22vh] flex flex-col md:flex-row items-center md:justify-between md:items-center !px-6 md:!px-4 !py-4 md:!py-4'>
+        <section className='w-full h-fit xl:h-[22vh] flex flex-col xl:flex-row items-center xl:justify-between xl:items-center !px-6 xl:!px-4 !py-4 xl:!py-4'>
 
           {/* site name, about and email */}
-          <div className='w-full md:w-4/12'>
+          <div className='w-full xl:w-4/12'>
             <a href='#' className='site-title !text-gray-500 hover:!text-red-400'>TechVolt</a>
             <p className='!my-4 md:!my-0'>
               TechVolt: Unleashing Innovation, Redefining Convenience &#45; Your Ultimate Destination for Seamless Tech Exploration and Empowerment.
@@ -26,7 +26,7 @@ export default function FooterComponent() {
           </div>
 
           {/* sitemap */}
-          <div className='w-full md:w-fit flex flex-col !items-start'>
+          <div className='w-full xl:w-fit flex flex-col !items-start'>
             <h1>Sitemap</h1>
             <a href="#">Product</a>
             <a href="#">Services</a>
@@ -35,7 +35,7 @@ export default function FooterComponent() {
           </div>
 
           {/* Information */}
-          <div className='w-full md:w-fit flex flex-col !items-start'>
+          <div className='w-full xl:w-fit flex flex-col !items-start'>
             <h1>Information</h1>
             <a href="#">Privacy Policy</a>
             <a href="#">Terms & Conditions</a>
@@ -44,11 +44,11 @@ export default function FooterComponent() {
           </div>
 
           {/* socials */}
-          <div className='w-full md:w-fit flex !flex-col !justify-start !items-start'>
+          <div className='w-full xl:w-fit flex !flex-col !justify-start !items-start'>
 
             <h1>Connect with us</h1>
 
-            <div className='w-6/12 md:w-full footer-socials flex justify-between items-center !mt-4'>
+            <div className='w-6/12 md:w-4/12 lg:w-2/12 xl:w-full flex justify-between items-center !mt-4'>
               <a href="#"><CiInstagram /></a>
               <a href="#"><RiTwitterXFill /></a>
               <a href="#"><CiLinkedin /></a>
