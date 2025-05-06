b156efef2c76b59e743a3848d3d1a98d27233d16
BlckTitan <dorshman406@gmail.com>
2025-05-06 14:02:25 +0100

    feat(productDetails):Built the deader section for the product details page

--- src/app/productDetails/page.jsx
+++ src/app/productDetails/page.jsx
@@ -14,13 +14,36 @@ export default function ProductDetails() {
   return (
     <section className='w-full h-fit flex flex-col justify-between'>
         <Navigation subLink={subLink}/>
+
+        {/* Product details header page */}
+        <header className='w-full h-30 flex justify-center items-center bg-background-gray'>
+            <div className='w-full xl:w-9/12 h-fit flex flex-col xl:flex-row justify-between items-start '>
+
+                <section className='w-1/2 h-full'>
+                    <p className='text-lg xl:text-2xl font-bold'>7Kg Front Load Washing Machine, Inverter Direct Drive, Middle Black</p>
+                </section>
+
+                <section className='w-1/2 h-full flex justify-end items-center'>
+                    <div className='text-end'>
+                        <p className='font-semibold text-xl'>₹26490.00</p>  
+                        <p className='text-lg'> 
+                            MRP (Inclusive of all taxes) 
+                            <strike className='!mx-3'> ₹40990.00 </strike> 
+                            <span className='text-green-600'>Save ₹14500.00</span>
+                        </p>
+                    </div>
+
+                    <button className='action-button bg-red-500 !ml-2'>Add to Cart</button>
+
+                </section>
+            </div>
+        </header>
         
         <section className='w-full h-fit !py-6 xl:h-[calc(100vh-13rem) bg-white flex flex-col xl:flex-row justify-center items-center !px-6 xl:!px-0'>
         
             <div className='w-full xl:w-9/12 h-fit flex flex-col xl:flex-row justify-between items-start '>
 
             <main className='w-full xl:w-9/12'>
-            gjg
             </main>
 
             </div>
